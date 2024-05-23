214222002b5d12751c7007cf25d39361b60d8b58
janpdevops <janp.dev@googlemail.com>
2024-05-23 18:53:51 +0200

    Check for known_hosts and prevent man-in-the-middle attacks.

--- client/scripts/rsync2backup.sh
+++ client/scripts/rsync2backup.sh
@@ -26,8 +26,20 @@ if ! [ -f ssh_key ]; then
 	# create add-user-skript, pass it to rsync-server
 	# add run script for user @runlevel
 else 
+# check for a know-hosts file and use it
+# Check for known_hosts and prevent man-in-the-middle attacks.
+# Copy the .ssh folder to permanent storage
+# Copy it back, if present
+# enable strict host checking (do not add keys, after the first has been added)
+# to test this feature: point to an ssh server that is not ours (e.g. spin up another container)
+STRICT_HOST_CHECKING="no"
+if  [[ -d .ssh && -f .ssh/known_hosts ]]; then
+	cp -r .ssh ~
+	STRICT_HOST_CHECKING="yes"
+fi
+
 # upload the files
 # 1s stage ingnore host checking TODO probably check for know-hosts and save it in the key folder
- 	 rsync -av --delete -e "ssh -i /data/sshkeys/ssh_key  -o \"StrictHostKeyChecking no\"  -p ${RSYNC_PORT} " /upload ${USER_NAME}@${RSYNC_SERVER}:/data
-	 cp -r ~/.ssh .
+ 	rsync -av --delete -e "ssh -i /data/sshkeys/ssh_key  -o \"StrictHostKeyChecking ${STRICT_HOST_CHECKING}\"  -p ${RSYNC_PORT} " /upload ${USER_NAME}@${RSYNC_SERVER}:/data
+	cp -r ~/.ssh .
 fi
